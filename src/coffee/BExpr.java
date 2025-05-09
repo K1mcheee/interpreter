238ffd29cd0960c934712a26a6126b23268a3b57
Kimberly <kimberlychiengyj@gmail.com>
2025-05-09 17:47:09 +0800

    add BExpr class

---
+++ src/coffee/BExpr.java
@@ -0,0 +1,34 @@
+package coffee;
+
+public class BExpr {
+    public static class NOT extends BExpr {
+        private BExpr arg;
+
+        public NOT(BExpr arg) {
+            this.arg = arg;
+        }
+
+    }
+
+    public static class AND extends BExpr {
+        private BExpr lhs;
+        private BExpr rhs;
+
+        public AND(BExpr lhs, BExpr rhs) {
+            this.lhs = lhs;
+            this.rhs = rhs;
+        }
+    }
+
+    public static class OR extends BExpr {
+        private BExpr lhs;
+        private BExpr rhs;
+
+        public OR(BExpr lhs, BExpr rhs) {
+            this.lhs = lhs;
+            this.rhs = rhs;
+        }
+    }
+
+
+}
