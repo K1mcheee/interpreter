1335d8540345b7f858306ca8968c48abe9f8aa06
Kimberly <kimberlychiengyj@gmail.com>
2025-05-10 17:56:48 +0800

    Add statement class

---
+++ src/coffee/Stmt.java
@@ -0,0 +1,91 @@
+package coffee;
+
+import java.util.List;
+
+import coffee.Expr.*;
+
+public abstract class Stmt {
+    public static class ASSG extends Stmt {
+        private VAR lhs;
+        private Expr rhs;
+
+        public ASSG(VAR lhs, Expr rhs) {
+            this.lhs = lhs;
+            this.rhs = rhs;
+        }
+
+        VAR lhs() {
+            return this.lhs;
+        }
+        Expr rhs() {
+            return this.rhs;
+        }
+    }
+
+    public static class DECL extends Stmt {
+        private VAR lhs;
+        private Expr rhs;
+
+        public DECL(VAR lhs, Expr rhs) {
+            this.lhs = lhs;
+            this.rhs = rhs;
+        }
+
+        VAR lhs() {
+            return this.lhs;
+        }
+        Expr rhs() {
+            return this.rhs;
+        }
+    }
+
+    public static class IF extends Stmt {
+        private BExpr cond;
+        private Stmt thenStmt;
+        private Stmt elseStmt;
+
+        public IF(BExpr cond, Stmt thenStmt, Stmt elseStmt) {
+            this.cond = cond;
+            this.thenStmt = thenStmt;
+            this.elseStmt = elseStmt;
+        }
+    }
+
+    public static class WHILE extends Stmt {
+        private BExpr cond;
+        private Stmt loop;
+
+        public WHILE(BExpr cond, Stmt loop) {
+            this.cond = cond;
+            this.loop = loop;
+        }
+    }
+
+    public static class BLOCK extends Stmt {
+        private List<Stmt> stmts;
+
+        public BLOCK(List<Stmt> stmts) {
+            this.stmts = stmts;
+        }
+    }
+
+    public static class FUNC extends Stmt {
+        private String name;
+        private List<VAR> pars;
+        private Stmt body;
+
+        public FUNC(String name, List<VAR> pars, Stmt body) {
+            this.name = name;
+            this.pars = pars;
+            this.body = body;
+        }
+    }
+
+    public static class RET extends Stmt {
+        private Expr expr;
+
+        public RET(Expr expr) {
+            this.expr = expr;
+        }
+    }
+}
